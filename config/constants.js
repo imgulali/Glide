b920130a3b565854aefeb8a1f1d47f17423f4e2b
imgulali <gulali.official@gmail.com>
2024-04-23 06:20:40 +0500

    Removed: dotenv configuration

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,2 @@
-import { configDotenv } from "dotenv";
-configDotenv();
-
-export const PORT = process.env.PORT || 4500;
-export const Chrome_Path = process.env.Chrome_Path || "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+export const PORT = 4500;
+export const Chrome_Path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
